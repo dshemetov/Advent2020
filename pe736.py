5bc8e5518f48f4416ef5cbec274b06f9bba721bb
Dmitry Shemetov <dshemetov@ucdavis.edu>
2020-12-07 23:42:58 -0800

    More examples

--- pe736.py
+++ pe736.py
@@ -5,18 +5,11 @@ def get_p(p, pt):
         (x, y) = (x + 1, 2 * y) if f == "r" else (2 * x, y + 1)
     return (x, y)
 
-
-path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
-get_p(path, (45, 90))
-
 path = list(["r", "s", "s", "r"])
 get_p(path, (45, 90))
 
 path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
 get_p(path, (45, 90))
-
-path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
-get_p(path, (0, 1))
-
-path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
-get_p(path, (45, 90))
+get_p(path, (45, 45 + 45))
+get_p(path, (45, 45))
+get_p(path, (0, 45))[1]
