6d702bdd7004021de6303208ae4d97bb52d3c9b2
Dmitry Shemetov <dshemetov@ucdavis.edu>
2020-12-07 22:32:22 -0800

    Examples

--- pe736.py
+++ pe736.py
@@ -1,12 +1,22 @@
 # Project Euler 736
 def get_p(p, pt):
-    x,y = pt
+    x, y = pt
     for f in reversed(p):
-        (x,y) = (x+1, 2*y) if f == "r" else (2*x, y+1)
-    return (x,y)
+        (x, y) = (x + 1, 2 * y) if f == "r" else (2 * x, y + 1)
+    return (x, y)
 
-path = list(reversed(["r", "s", "s", "s", "s", "r", "s", "r", "r"]))
-get_p(path, (45,90))
+
+path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
+get_p(path, (45, 90))
 
 path = list(["r", "s", "s", "r"])
-get_p(path, (45,90))
+get_p(path, (45, 90))
+
+path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
+get_p(path, (45, 90))
+
+path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
+get_p(path, (0, 1))
+
+path = list(["r", "r", "s", "r", "s", "s", "s", "s", "r"])
+get_p(path, (45, 90))
