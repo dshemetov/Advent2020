da329c7ae5add4c91178e40e63336f826ea258f1
Dmitry Shemetov <dshemetov@ucdavis.edu>
2020-12-08 01:02:43 -0800

    Shared to gist

--- pe736.py
+++ pe736.py
@@ -7,18 +7,8 @@ def get_p(p, pt):
         (x, y) = (x + 1, 2 * y) if f == "r" else (2 * x, y + 1)
     return (x, y)
 
-path = "rssr"
-get_p(path, (45, 90))
-
-path = "rrsrssssr"
-get_p(path, (45, 90))
-get_p(path, (45, 45 + 45))
-get_p(path, (45, 45))
-get_p(path, (0, 45))[1]
-
-
 # A* search algorithm
-def A_star(start_state, heuristic_distance):
+def A_star(start_state, heuristic_distance, choices):
     """
     Parameters
     ----------
@@ -33,28 +23,32 @@ def A_star(start_state, heuristic_distance):
         A list of states from start to goal.
     """
     priority_queue = []
-    current_distance = 0
     current_state = start_state
     current_path = ""
 
     while current_state[0] - current_state[1] != 0:
-        # for choice in "rs": 
-        for choice in ["rr", "rs", "sr", "ss"]:
+        # for choice in "rs":
+        for choice in choices:
             future_path = current_path + choice
-            distance = (
-                2
-                + current_distance
-                + heuristic_distance(future_path, start_state)
-            )
+            distance = heuristic_distance(future_path)
             heappush(priority_queue, (distance, future_path))
 
-        current_distance, current_path = heappop(priority_queue)
+        _, current_path = heappop(priority_queue)
         current_state = get_p(current_path, start_state)
 
     return current_path
 
-def rs_distance(path, start_state):
+def rs_distance(path):
     difference = path.count("s") - path.count("r")
-    return 4 * abs(1 - difference)
+    return abs(1 - difference) + len(path)
 
-A_star([45, 90], heuristic_distance=rs_distance)
+path = "rrsrssssr"
+print(get_p(path, (45, 90)))
+
+# finds the even length path hella fast
+path = A_star([45, 90], rs_distance, choices=["r", "s"])
+print(path)
+
+# odd length path; takes 10GB+
+path = A_star([45, 90], rs_distance, choices=["rr", "rs", "sr", "ss"])
+print(path)
